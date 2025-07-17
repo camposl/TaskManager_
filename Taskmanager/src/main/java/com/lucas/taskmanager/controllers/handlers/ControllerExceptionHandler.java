280f026ea171297b25f1d642e6f0d48bb61ee016
camposl <lucascampos25@gmail.com>
2025-07-16 22:32:21 -0300

    Ajuste tratamento de excoes do DB

--- Taskmanager/src/main/java/com/lucas/taskmanager/controllers/handlers/ControllerExceptionHandler.java
+++ Taskmanager/src/main/java/com/lucas/taskmanager/controllers/handlers/ControllerExceptionHandler.java
@@ -1,4 +1,34 @@
 package com.lucas.taskmanager.controllers.handlers;
 
+import com.lucas.taskmanager.dto.CustomError;
+import com.lucas.taskmanager.services.exceptions.DatabaseException;
+import com.lucas.taskmanager.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
 public class ControllerExceptionHandler {
+
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(request.getRequestURI(), e.getMessage(), status.value(), Instant.now());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(request.getRequestURI(), e.getMessage(), status.value(), Instant.now());
+        return ResponseEntity.status(status).body(err);
+    }
+
+
 }
--- Taskmanager/src/main/java/com/lucas/taskmanager/dto/CustomError.java
+++ Taskmanager/src/main/java/com/lucas/taskmanager/dto/CustomError.java
@@ -1,4 +1,37 @@
 package com.lucas.taskmanager.dto;
 
+import java.time.Instant;
+
 public class CustomError {
+
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+
+    public CustomError() {
+    }
+
+    public CustomError(String path, String error, Integer status, Instant timestamp) {
+        this.path = path;
+        this.error = error;
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getPath() {
+        return path;
+    }
 }
